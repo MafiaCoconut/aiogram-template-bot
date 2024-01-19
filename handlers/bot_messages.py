d0f937dd88fb523c2fa8738eae5bcc30880c14ea
MafiaCoconut <alex.kach.2000@gmail.com>
2024-01-19 02:09:01 +0100

    бот готов

--- handlers/bot_messages.py
+++ handlers/bot_messages.py
@@ -12,12 +12,10 @@ router = Router()
 
 @router.message()
 # @router.message(F.content_type.in_({'sticker'}))
-async def echo_handler(message: Message, language: str) -> None:
+async def echo_handler(message: Message) -> None:
     """Функция вывода заглушки на необъявленное сообщение/команду"""
-
-    l10n = auxiliary.get_l10n(language)
     send_log(message.text, message.chat.username)
-    await message.answer(l10n.format_value('echo'), reply_markup=inline.get_send_menu_main(l10n))
+    await message.answer("Команды не найдено. Повторите попытку ввода.")
 
 
 
--- utils/commands.py
+++ utils/commands.py
@@ -7,6 +7,10 @@ async def set_commands(bot: Bot):
         BotCommand(
             command='main_menu',
             description='Главное меню'
+        ),
+        BotCommand(
+            command='help',
+            description='Помощь'
         )
     ]
 
